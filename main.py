3b43741fc6b25629701c905fb567a6d68f940321
Mohamed Abdi <mkabdi27@gmail.com>
2017-04-11 20:31:14 -0500

    made changes

--- main.py
+++ main.py
@@ -51,94 +51,85 @@ def valid_email(email):
 
 class Index(webapp2.RequestHandler):
     def get(self):
-
-
-
-        header = "<h1>Signup</h1>"
-        add_form = """
-        <form action="/welcome" method="post">
-            <table>
-                <tr>
-                    <td><label for="username">Username</label></td>
-                    <td>
-                        <input name="username" type="text" value="">
-                        <span class="error" id="username"></span>
-                    </td>
-                </tr>
-                <tr>
-                    <td><label for="password">Password</label></td>
-                    <td>
-                        <input name="password" type="password">
-                        <span class="error" id="password"></span>
-                    </td>
-                </tr>
-                <tr>
-                    <td><label for="verify">Verify Password</label></td>
-                    <td>
-                        <input name="verify" type="password">
-                        <span class="error" id="verify"></span>
-                    </td>
-                </tr>
-                <tr>
-                    <td><label for="email">Email (optional)</label></td>
-                    <td>
-                        <input name="email" type="email" value="">
-                        <span class="error" id="email" ></span>
-                    </td>
-                </tr>
-            </table>
-            <input type="submit" value="Submit">
-        </form>
-        """
-        error = self.request.get("error")
-        if error:
-            error_esc = cgi.escape(error, quote=True)
-            error_element = '<span class="error" id="">' + error_esc + '</span>'
-        else:
-           error_element = ''
-
-
-        main_content = header + add_form + error_element
-        content = page_header + main_content + page_footer
-        self.response.write(content)
-
-class Welcome(webapp2.RequestHandler):
-
-    def post(self):
-
-
+        have_error = False
         username = self.request.get('username')
         password = self.request.get('password')
         verify = self.request.get('verify')
         email = self.request.get('email')
 
+        params = dict(username = username, email = email)
+
         if not valid_username(username):
-            error = "That's not a valid username."
-            self.redirect("/?error=" + error)
+            params["error_username"] = "That's not a valid username."
+            have_error = True
 
         if not valid_password(password):
-            error = "That wasn't a valid password."
-            self.redirect("/?error=" + error)
+            params["error_password"] = "That wasn't a valid password."
+            have_error = True
         elif password != verify:
-            error = "Your passwords didn't match."
-            self.redirect("/?error=" + error)
+            params["error_verify"] = "Your passwords didn't match."
+            have_error = True
 
         if not valid_email(email):
-            error = "That's not a valid email."
-            self.redirect("/?error=" + error)
+            params["error_email"] = "That's not a valid email."
+            have_error = True
 
 
 
 
 
+        add_form = """
+        <h1>Signup</h1>
+        <form method="post" action="/welcome">
+            <table>
+                <tr>
+                    <td class="label">Username</td>
+                    <td>
+                        <input name="username" type="text" value="">
+                    </td>
+                        <td class="error">%{error_username}
+                    </td>
+                </tr>
+                <tr>
+                    <td class="label">Password</td>
+                    <td>
+                        <input name="password" type="password">
+                    </td>
+                        <td class="error">{error_password}
+                    </td>
+                </tr>
+                <tr>
+                    <td class="label">Verify Password</td>
+                    <td>
+                        <input name="verify" type="password">
+                    </td>
+                        <td class="error>{error_verify}
+                    </td>
+                </tr>
+                <tr>
+                    <td class="label">Email (optional)</td>
+                    <td>
+                        <input name="email" type="email" value="">
+                    </td>
+                        <td class="error">{error_email}
+                    </td>
+                </tr>
+            </table>
+            <input type="submit" value="Submit">
+        </form>
+        """.format(params)
 
-        username = cgi.escape(username)
-        password = cgi.escape(password)
-        verify = cgi.escape(verify)
-        email = cgi.escape(email)
+        if have_error:
+            self.redirect("/", params)
+        else:
+            self.redirect("/welcome?username=" + username)
 
 
 
+
+class Welcome(webapp2.RequestHandler):
+
+    def post(self):
 
         sentence = "Welcome, " + username + "!"
         content = page_header + "<h1>" + sentence + "</h1>" + page_footer
